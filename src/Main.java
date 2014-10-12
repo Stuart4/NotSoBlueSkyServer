805d230be01c1d32fc5b18a57edd437fcff8b776
Jacob Stuart <stuart4@purdue.edu>
2014-10-11 20:43:42 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.net.Socket;
+
+/**
+ * NotSoBlueSkyServer
+ *
+ * @author Jacob Stuart
+ * @lab 806
+ * @date October 11, 2014
+ */
+public class Main {
+	public static void main(String[] args) {
+		Socket s = new Socket();
+	}
+}
---
+++ src/Worker.java
@@ -0,0 +1,14 @@
+import java.net.Socket;
+
+/**
+ * NotSoBlueSkyServer
+ *
+ * @author Jacob Stuart
+ * @lab 806
+ * @date October 11, 2014
+ */
+public class Worker {
+	public Worker(Socket connection) {
+		
+	}
+}
